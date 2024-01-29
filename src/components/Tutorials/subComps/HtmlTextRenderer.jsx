afb824ae892355488c7b70b8f3ac39aed3da1160
Aditya Sakpal <112710558+Aditya-Sakpal@users.noreply.github.com>
2024-01-29 23:59:13 +0530

    Update HtmlTextRenderer.jsx

--- src/components/Tutorials/subComps/HtmlTextRenderer.jsx
+++ src/components/Tutorials/subComps/HtmlTextRenderer.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import DOMPurify from "dompurify";
 import "../../../css/quillEditor.css";
-// import "quill/dist/quill.snow.css";
 
 const HtmlTextRenderer = ({ html = "<p>Html Text Renderer</p>" }) => {
   // used to remove any sensitive tags like <script> which might me malicious
