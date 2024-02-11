4af2ab5add76f9d20f2601b1b032d7f7b577ee91
Aditya-Sakpal <aditya.as@somaiya.edu>
2024-02-11 11:13:12 +0530

    Confirmation

--- src/components/Editor/QuillEditor.jsx
+++ src/components/Editor/QuillEditor.jsx
@@ -72,25 +72,36 @@ const QuillEditor = ({ id, data, tutorial_id }) => {
       const editor = new Quill(editorRef.current, {
         modules: {
           cursors: true,
-          toolbar: [
-            ["bold", "italic", "underline", "strike"], // toggled buttons
-            ["blockquote", "code-block"],
-
-            [{ header: 1 }, { header: 2 }], // custom button values
-            [{ list: "ordered" }, { list: "bullet" }],
-            [{ script: "sub" }, { script: "super" }], // superscript/subscript
-            [{ indent: "-1" }, { indent: "+1" }], // outdent/indent
-            [{ direction: "rtl" }], // text direction
-
-            [{ size: ["small", false, "large", "huge"] }], // custom dropdown
-            [{ header: [1, 2, 3, 4, 5, 6, false] }],
-
-            [{ color: [] }, { background: [] }], // dropdown with defaults from theme
-            [{ font: [] }],
-            [{ align: [] }],
-            ["clean"], // remove formatting button
-            ["link", "image"]
-          ],
+          toolbar: {
+            container: [
+              ["bold", "italic", "underline", "strike"], // toggled buttons
+              ["blockquote", "code-block"],
+  
+              [{ header: 1 }, { header: 2 }], // custom button values
+              [{ list: "ordered" }, { list: "bullet" }],
+              [{ script: "sub" }, { script: "super" }], // superscript/subscript
+              [{ indent: "-1" }, { indent: "+1" }], // outdent/indent
+              [{ direction: "rtl" }], // text direction
+  
+              [{ size: ["small", false, "large", "huge"] }], // custom dropdown
+              [{ header: [1, 2, 3, 4, 5, 6, false] }],
+  
+              [{ color: [] }, { background: [] }], // dropdown with defaults from theme
+              [{ font: [] }],
+              [{ align: [] }],
+              ["clean"], // remove formatting button
+              ["link", "image"]
+            ],
+            handlers:{
+              link: function() {
+                var range = this.quill.getSelection();
+                var value = prompt('Enter image URL here');
+                if(value) {
+                  this.quill.insertEmbed(range.index, 'image', value, Quill.sources.USER);
+                }
+              }
+            }
+          },
           history: {
             userOnly: true
           }
