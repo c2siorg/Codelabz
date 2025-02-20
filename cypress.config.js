f571d2d9db4da0eef886d8b64c4db0c7f2457782
Mallepally Lokeshwar Reddy <lokeshwar777@gmail.com>
2025-02-20 12:25:04 +0530

    chore: simplify and adjust gihub workflows and secrets

--- cypress.config.js
+++ cypress.config.js
@@ -8,7 +8,8 @@ export default defineConfig({
     baseUrl: "http://localhost:5173",
     testIsolation: false
   },
-  projectId: process.env.CYPRESS_PROJECT_ID,
-  record: true, // Enable test recording
-  key: process.env.CYPRESS_RECORD_KEY // Cypress record key
+  projectId: process.env.CYPRESS_PROJECT_ID
+  // uncomment these after the secrets are configured
+  // record: true, // Enable test recording
+  // key: process.env.CYPRESS_RECORD_KEY // Cypress record key
 });
