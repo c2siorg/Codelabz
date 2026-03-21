1a415b67292e07d7021163fddf785461b5c643e4
N <suchitraaradhya37@gmail.com>
2026-03-21 15:31:10 +0530

    fix: correct Firebase emulator env variable detection in config

--- src/config/index.js
+++ src/config/index.js
@@ -29,7 +29,7 @@ export const onlineFirebaseApp = initializeApp(firebaseConfig, "secondary");
 firebase.initializeApp(firebaseConfig);
 const db = firebase.firestore();
 
-if (import.meta.env.VITE_APP_USE_EMULATOR === "true") {
+if (import.meta.env.VITE_APP_USE_EMULATOR) {
   console.log("Using emulator");
   firebase.firestore().useEmulator("localhost", 8080);
   firebase
