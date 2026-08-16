ec4c18201f98cad7a470393d3db924bb76fbc9ec
gautamsidhwani29 <gautamsidhwani405@gmail.com>
2026-08-16 18:14:41 +0530

    fix: build tutorial search index on demand instead of at app start

--- src/store/actions/index.js
+++ src/store/actions/index.js
@@ -87,7 +87,7 @@ export {
   setTutorialTheme,
   updateStepTime,
   updateStepTitle,
-  fetchAndIndexTutorials,
+  ensureTutorialsIndexed,
   updateTagFrequencies,
   uploadTutorialImages
 } from "./tutorialsActions";
